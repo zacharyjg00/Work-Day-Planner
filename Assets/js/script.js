b369d31fef689273887007e6b82f94ec5fb4ede4
Zacharyjg <guldb016@umn.edu>
2021-06-21 19:36:52 -0500

    Formatted and commented code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,15 @@
+// Instantiate the moment object to display todays date as well as grab the current hour from the moment object
 let today = moment();
 let hour = today.format("H");
 
+// Sets the current date in the jumbotron
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+// This function goes through and dynamically generates all of the table with the appropriate times and colors
 function createHTML() {
+    // For loop is set from 9 to 18 as it dynamically sets up the times in the first column while still iterating 9 times
     for (let i = 9; i < 18; i++) {
+        // Declarations of all of the tags that need to be created
         let row = $("<div>");
         let col1 = $("<div>");
         let col2 = $("<textarea>");
@@ -12,23 +17,9 @@ function createHTML() {
         let col3Glyph = $("<i>");
 
         row.attr("class", "row");
+
+        // This set of if statements sets up the first columns with the appropriate times and labels such as AM or PM
         col1.attr("class", "col-1 hour");
-        col2.attr("id", "textarea" + (i - 9));
-
-        if (hour > i) {
-            col2.attr("class", "past col-10");
-        } else if (hour == i) {
-            col2.attr("class", "present col-10");
-        } else {
-            col2.attr("class", "future col-10");
-        }
-
-        col3.attr("class", "col-1 saveBtn");
-        col3.attr("id", "button" + (i - 9));
-
-        col3Glyph.attr("class", "fas fa-save");
-        col3Glyph.attr("id", "glyph" + (i - 9));
-
         if (i < 13) {
             if (i == 12) {
                 col1.text(i + "PM");
@@ -39,6 +30,23 @@ function createHTML() {
             col1.text((i - 12) + "PM");
         }
 
+        // This set of if statements sets up the second column with the dynamic coloring based on the current time of day
+        col2.attr("id", "textarea" + (i - 9));
+        if (hour > i) {
+            col2.attr("class", "past col-10");
+        } else if (hour == i) {
+            col2.attr("class", "present col-10");
+        } else {
+            col2.attr("class", "future col-10");
+        }
+
+        // This section generated the third column and assigns classes as well as sets up the icon in the button
+        col3.attr("class", "col-1 saveBtn");
+        col3.attr("id", "button" + (i - 9));
+        col3Glyph.attr("class", "fas fa-save");
+        col3Glyph.attr("id", "glyph" + (i - 9));
+
+        // This section append all of the tags onto the container given in the html
         col3.append(col3Glyph);
         $(".container").append(row);
         row.append(col1);
@@ -47,24 +55,28 @@ function createHTML() {
     }
 }
 
+// This function sets up the keys and values that are set in localStorage where the row number is passed in to differentiate the data
 function saveToLocalStorage(rowNum) {
     let localStorageName = "textarea" + rowNum;
     let localStorageValue = "#textarea" + rowNum;
     localStorage.setItem(localStorageName, $(localStorageValue).val());
 }
 
+// This function goes through the localStorage and populates the textareas with the appropriate text that was saved
 function populateTextareas() {
-    for(let i = 0; i < localStorage.length; i++) {
+    for (let i = 0; i < localStorage.length; i++) {
         let textareaId = "#" + localStorage.key(i);
         let textareaKey = localStorage.key(i);
         $(textareaId).text(localStorage.getItem(textareaKey));
     }
 }
 
+// The calls of the functions to generate the HTML and populate the texareas
 createHTML();
 populateTextareas();
 
-$(".saveBtn, .fas").on("click",  function(event) {
+// This is the click handler for the buttons which waits until the user click on the button or the icon and calls saveToLocalStorage() to save the text
+$(".saveBtn, .fas").on("click", function (event) {
     event.preventDefault();
     let buttonId = event.target.id;
     saveToLocalStorage(buttonId.slice(-1));
